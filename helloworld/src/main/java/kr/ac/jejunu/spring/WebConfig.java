922c62d00baf32dbe4d643340cb829b06181d385
Lming1 <spear131121@gmail.com>
2018-05-25 16:33:44 +0900

    이전 커밋메세지 x, ㅇdispatcher-servlet bye

--- helloworld/src/main/java/kr/ac/jejunu/spring/WebConfig.java
+++ helloworld/src/main/java/kr/ac/jejunu/spring/WebConfig.java
@@ -7,15 +7,29 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "kr.ac.jejunu")
 public class WebConfig implements WebMvcConfigurer {
     @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        if(configurer.getUrlPathHelper() == null) {
+            configurer.setUrlPathHelper(new UrlPathHelper());
+        }
+        configurer.getUrlPathHelper().setRemoveSemicolonContent(false);
+    }
+
+    @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.mediaType("js", MediaType.APPLICATION_JSON);
         configurer.mediaType("x", MediaType.APPLICATION_XML);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HelloInterceptor());
     }
 
     @Override
