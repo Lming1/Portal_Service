c8297deec4b1ff33bf2e62f4836d57dfb465993e
Lming1 <spear131121@gmail.com>
2018-05-25 16:27:58 +0900

    web.xml bye

--- helloworld/src/main/java/kr/ac/jejunu/spring/RestUserController.java
+++ helloworld/src/main/java/kr/ac/jejunu/spring/RestUserController.java
@@ -1,10 +1,35 @@
 package kr.ac.jejunu.spring;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import kr.ac.jejunu.hello.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest")
 public class RestUserController {
+    @GetMapping("{id}")
+    public User get(@PathVariable Integer id){
+        User user = new User();
+        user.setId(id);
+        user.setName("ming");
+        user.setPassword("1234");
+        return user;
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return user;
+    }
+
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        return user;
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Integer id){
+
+    }
+
 }
