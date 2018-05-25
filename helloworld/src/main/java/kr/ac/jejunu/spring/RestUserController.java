913a9b06a416f44520f51d4cfb199c604fb555cc
Lming1 <spear131121@gmail.com>
2018-05-25 16:09:27 +0900

    location change

---
+++ helloworld/src/main/java/kr/ac/jejunu/spring/RestUserController.java
@@ -0,0 +1,10 @@
+package kr.ac.jejunu.spring;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class RestUserController {
+}
