da955e6800c609160ed807ee696b925adcedd0eb
Lming1 <spear131121@gmail.com>
2018-06-01 16:08:41 +0900

    rest crud

---
+++ project/src/main/java/kr/ac/jejunu/project/UserController.java
@@ -0,0 +1,44 @@
+package kr.ac.jejunu.project;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User get(@PathVariable Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+
+    @GetMapping("/list")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+
+    @PutMapping
+    public void modify(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        userRepository.delete(userRepository.findById(id).get());
+    }
+
+
+}
